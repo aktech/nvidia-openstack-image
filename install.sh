596a695fa27346603727fcb515d68bb99873e4b9
Amit Kumar <dtu.amit@gmail.com>
2022-11-10 00:48:34 +0000

    download image

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 sudo apt install python3-pip
 sudo apt-get install qemu-utils
 pip3 install diskimage-builder
+
+wget -P /tmp/ https://storage.googleapis.com/akpublic/MLNX_OFED_LINUX-5.8-1.0.1.1-ubuntu22.10-x86_64.iso
