0db074782b2aad6c3a77db3b90c4b14ba4b2af80
Amit Kumar <dtu.amit@gmail.com>
2022-11-11 14:56:04 +0000

    fix environment variables and ubuntu release

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install python3-pip
-sudo apt-get install qemu-utils
+sudo apt install python3-pip -y
+sudo apt-get install qemu-utils -y
+
+python3 -m pip install --user --upgrade pip
+sudo apt install python3-venv
+
+python3 -m venv venv
+source venv/bin/activate
 pip3 install diskimage-builder
 
 wget -P /tmp/ https://storage.googleapis.com/akpublic/MLNX_OFED_LINUX-5.8-1.0.1.1-ubuntu22.10-x86_64.iso
