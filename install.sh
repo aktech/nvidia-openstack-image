4130b7b90ab92668091e7621ca24da93a45be0b6
Amit Kumar <dtu.amit@gmail.com>
2022-11-11 17:37:53 +0000

    remove mofed

--- install.sh
+++ install.sh
@@ -9,5 +9,3 @@ sudo apt install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install diskimage-builder
-
-wget -P /tmp/ https://storage.googleapis.com/akpublic/MLNX_OFED_LINUX-5.8-1.0.1.1-ubuntu22.04-x86_64.iso
