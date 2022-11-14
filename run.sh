ed5f64a354e7cb421fa5250b48f81c533377f0bc
Amit Kumar <dtu.amit@gmail.com>
2022-11-14 15:50:08 +0000

    ignore minimal for now

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ timestamp=$(date +%s)
 OUTPUT_IMAGE=/home/ubuntu/ubuntu-nvidia-$timestamp
 
 disk-image-create vm dhcp-all-interfaces \
-  block-device-gpt ubuntu-minimal \
+  block-device-gpt ubuntu \
   cuda -x \
   --no-tmpfs \
   --logfile dib.log \
