bdadecdd9e7fec2eb5ea200da418d2584dde1a09
Amit Kumar <dtu.amit@gmail.com>
2022-11-15 22:33:10 +0000

    fix logging

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 source .env
-#timestamp=$(date +%s)
-#OUTPUT_IMAGE=/home/ubuntu/ubuntu-nvidia-$timestamp
+timestamp=$(date +%s)
+
+#if [[ -z "${SET_LOGGING}" ]]; then SET_LOGGING="--logfile dib.log"; fi
+if [[ -z "${OUTPUT_IMAGE}" ]]; then OUTPUT_IMAGE="/home/ubuntu/ubuntu-nvidia-$timestamp"; fi
 
 disk-image-create vm dhcp-all-interfaces \
   block-device-gpt ubuntu \
   cuda -x \
   --no-tmpfs \
-  --logfile dib.log \
   -o "$OUTPUT_IMAGE"
