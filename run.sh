12a866f4fd05fed597eb45318f26cfc4af7a2f01
Amit Kumar <dtu.amit@gmail.com>
2023-01-03 21:12:51 +0530

    Build nvidia 525

--- run.sh
+++ run.sh
@@ -7,6 +7,5 @@ if [[ -z "${OUTPUT_IMAGE}" ]]; then OUTPUT_IMAGE="/home/runnerx/nvidia-openstack
 disk-image-create vm dhcp-all-interfaces \
   block-device-gpt ubuntu \
   cuda -x \
-  misc \
   --no-tmpfs \
   -o "$OUTPUT_IMAGE"
