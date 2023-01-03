ec600106b23a2c2389bd9502ca6a674386ae03f3
Amit Kumar <dtu.amit@gmail.com>
2023-01-03 13:26:51 +0530

    fix output image path [skip ci]

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ source .env
 timestamp=$(date +%s)
 
 #if [[ -z "${SET_LOGGING}" ]]; then SET_LOGGING="--logfile dib.log"; fi
-if [[ -z "${OUTPUT_IMAGE}" ]]; then OUTPUT_IMAGE="/home/ubuntu/ubuntu-nvidia-$timestamp"; fi
+if [[ -z "${OUTPUT_IMAGE}" ]]; then OUTPUT_IMAGE="/home/runnerx/nvidia-openstack-image-$timestamp"; fi
 
 disk-image-create vm dhcp-all-interfaces \
   block-device-gpt ubuntu \
