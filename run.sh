5a601eae2e70a4fa3ae2395c499eb6a849de8d9f
Amit Kumar <dtu.amit@gmail.com>
2023-01-05 19:27:42 +0530

    add log to file option and use jammy

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 source .env
 timestamp=$(date +%s)
 
-#if [[ -z "${SET_LOGGING}" ]]; then SET_LOGGING="--logfile dib.log"; fi
+if [[ -z "${LOG_TO_FILE}" ]]; then LOG_TO_FILE="--logfile dib.log"; fi
 if [[ -z "${OUTPUT_IMAGE}" ]]; then OUTPUT_IMAGE="/home/runnerx/nvidia-openstack-image-$timestamp"; fi
 
+echo "Starting Disk Image builder"
 disk-image-create vm dhcp-all-interfaces \
   block-device-gpt ubuntu \
   cuda -x \
-  --no-tmpfs \
+  --no-tmpfs "$LOG_TO_FILE" \
   -o "$OUTPUT_IMAGE"
+echo "Starting Disk Image Finished"
